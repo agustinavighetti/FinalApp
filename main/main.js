142f9f7004410038d7a7cb021045dce3746331f4
Agustina Vighetti <agustinavighetti@yahoo.com.ar>
2016-10-14 11:18:21 -0300

    [Agustina Vighetti] - Final App

---
+++ main/main.js
@@ -0,0 +1,24 @@
+var myApp = angular.module('myApp');
+
+myApp.factory('SelectedPlace', function () {
+    return {place: 'holi'}
+});
+
+myApp.controller('MainController', ['$scope', 'SelectedPlace', function ($scope, SelectedPlace) {
+    let place, newLatitude, newLength, map, ac, newPlace;
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -32.9442426, lng: -60.65053879999999},
+      zoom: 13
+    });
+    ac = new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
+    
+    google.maps.event.addListener(ac, 'place_changed', function() {
+        place = ac.getPlace();
+        newLatitude = place.geometry.location.lat();
+        newLength = place.geometry.location.lng();
+        newPlace = new google.maps.LatLng(newLatitude, newLength);
+        map.setCenter(newPlace);
+        SelectedPlace.place = newPlace;
+    });
+}]);
